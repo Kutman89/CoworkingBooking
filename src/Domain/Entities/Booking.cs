39cc08cbbf88226d73453b6f5ff9c46b97c4a55f
Kutman89 <abdalievkutman77@gmail.com>
2026-08-02 20:32:52 +0600

    Добавьте файлы проекта.

---
+++ src/Domain/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Booking
+    {
+        public Guid id { get; set; }
+        public Guid roomId { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public BookingStatus Status { get; set; }
+    }
+}
---
+++ src/Domain/Enums/BookingStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+    public enum BookingStatus
+    {
+        Pending, // В ожидании
+        Confirmed, // Подтверждено
+        Cancelled, // Отменено
+        Completed // Завершено
+    }
+}
